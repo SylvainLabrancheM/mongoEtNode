3920c2225c76cda74662f190ed581cfe370096d4
Labranche <slabranche@cmontmorency.qc.ca>
2023-03-28 13:04:16 -0400

    liste utilisateurs

--- controllers/utilisateurs-controlleurs.js
+++ controllers/utilisateurs-controlleurs.js
@@ -1,70 +1,85 @@
-const {v4 : uuidv4} = require("uuid");
-const HttpErreur = require("../models/http-erreur")
+const { v4: uuidv4 } = require("uuid");
+const HttpErreur = require("../models/http-erreur");
 
 const Utilisateur = require("../models/utilisateur");
 
 const UTILISATEURS = [
-    {
-        id: "u1",
-        nom: "Sylvain Labranche",
-        courriel: "slabranche@cmontmorency.qc.ca",
-        motDePasse: "test"
-    }
-
+  {
+    id: "u1",
+    nom: "Sylvain Labranche",
+    courriel: "slabranche@cmontmorency.qc.ca",
+    motDePasse: "test",
+  },
 ];
 
-const getUtilisateurs = (requete, reponse, next) => {
-    reponse.json({utilisateurs: UTILISATEURS});
+const getUtilisateurs = async (requete, reponse, next) => {
+  let utilisateurs;
 
+  try {
+    utilisateurs = await Utilisateur.find({}, "-motDePasse");
+  } catch {
+    return next(new HttpErreur("Erreur accès utilisateurs"), 500);
+  }
+
+  reponse.json({
+    utilisateurs: utilisateurs.map(utilisateur =>
+      utilisateur.toObject({ getters: true })
+    ) });
 };
 
 const inscription = async (requete, reponse, next) => {
-    const {nom, courriel, motDePasse, places} = requete.body;
+  const { nom, courriel, motDePasse, places } = requete.body;
 
-    let utilisateurExiste;
+  let utilisateurExiste;
 
-    try{
-        utilisateurExiste = await Utilisateur.findOne({courriel:courriel});
-    }catch{
-       return next( new HttpErreur("Échec vérification utilisateur existe", 500));
-    }
+  try {
+    utilisateurExiste = await Utilisateur.findOne({ courriel: courriel });
+  } catch {
+    return next(new HttpErreur("Échec vérification utilisateur existe", 500));
+  }
 
-    if(utilisateurExiste){
-        return next(new HttpErreur("Utilisateur existe déjà, veuillez vos connecter", 422));
-    }
+  if (utilisateurExiste) {
+    return next(
+      new HttpErreur("Utilisateur existe déjà, veuillez vos connecter", 422)
+    );
+  }
 
-    let nouvelUtilisateur = new Utilisateur({
-        nom,
-        courriel,
-        image:"image.png",
-        motDePasse,
-        places
-    })
-    try{
-        await nouvelUtilisateur.save();
-    }catch(err){
-        console.log(err);
-        return next(new HttpErreur("Erreur lors de l'ajout de l'utilisateur", 422));
-    }
-    reponse.status(201).json({utilisateur:nouvelUtilisateur.toObject({getter:true})});
+  let nouvelUtilisateur = new Utilisateur({
+    nom,
+    courriel,
+    image: "image.png",
+    motDePasse,
+    places,
+  });
+  try {
+    await nouvelUtilisateur.save();
+  } catch (err) {
+    console.log(err);
+    return next(new HttpErreur("Erreur lors de l'ajout de l'utilisateur", 422));
+  }
+  reponse
+    .status(201)
+    .json({ utilisateur: nouvelUtilisateur.toObject({ getter: true }) });
 };
 
 const connexion = async (requete, reponse, next) => {
-    const {courriel, motDePasse} = requete.body;
+  const { courriel, motDePasse } = requete.body;
 
-    let utilisateurExiste;
+  let utilisateurExiste;
 
-    try{
-        utilisateurExiste = await Utilisateur.findOne({courriel:courriel});
-    }catch{
-       return next( new HttpErreur("Connexion échouée, veuillez réessayer plus tard", 500));
-    }
+  try {
+    utilisateurExiste = await Utilisateur.findOne({ courriel: courriel });
+  } catch {
+    return next(
+      new HttpErreur("Connexion échouée, veuillez réessayer plus tard", 500)
+    );
+  }
 
-    if(!utilisateurExiste || utilisateurExiste.motDePasse !== motDePasse){
-        return next( new HttpErreur("Courriel ou mot de passe incorrect", 401));
-    }
-    
-    reponse.json({message: "connexion réussie!"});
+  if (!utilisateurExiste || utilisateurExiste.motDePasse !== motDePasse) {
+    return next(new HttpErreur("Courriel ou mot de passe incorrect", 401));
+  }
+
+  reponse.json({ message: "connexion réussie!" });
 };
 
 exports.getUtilisateurs = getUtilisateurs;
